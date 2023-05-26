1288e9f7a33812c7e4bb8204ada5af6bf955f053
Siti-aminah <80364848+Siti-aminah@users.noreply.github.com>
2023-05-26 11:48:37 +0700

    Add files via upload

---
+++ page/home/index.php
@@ -0,0 +1,17 @@
+<section class="hero is-large is-success is-bold">
+    <div class="hero-body">
+        <div class="container">
+        <h1 class="title animate__animated animate__zoomIn">
+                Metode WP (Weighted Product)
+                </h1>
+            <h2 class="subtitle animate__animated animate__slideInUp">
+           Metode ini sering digunakan untuk membantu dalam menentukan goal atau keputusan.Metode Weighted Product(WP) menggunakan perkalian untuk menghubungkan rating atribut, dimana rating setiap
+atribut harus dipangkatkan dulu dengan bobot atribut yang bersangkutan. Proses tersebut sama halnya dengan
+normalisasi.
+Metode Weighted Product dapat membantu dalam mengambil keputusan akan tetapi perhitungan dengan
+menggunakan metode weighted product ini hanya menghasilkan nilai terbesar yang akan terpilih sebagai alternatif yang
+terbaik. Perhitungan akan sesuai dengan metode ini apabila alternatif yang terpilih memenuhi kriteria yang telah
+ditentukan.
+        </div>
+    </div>
+</section>
